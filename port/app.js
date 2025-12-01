c2fd67ad4554ba844e3f7658776bb9cd0915cca6
yllass <javier.zamudio251925@potros.itson.edu.mx>
2025-12-01 13:14:09 -0700

    Add files via upload

--- port/app.js
+++ port/app.js
@@ -1,6 +1,6 @@
 const API_URL = "https://portfolio-api-three-black.vercel.app/api/v1";
 
-// 🔥 TU ITSOn ID
+// 🔥 CAMBIA ESTE ITSOn ID AL TUYO
 const itsonId = "251925";
 
 document.addEventListener("DOMContentLoaded", cargarProyectos);
@@ -12,29 +12,20 @@ async function cargarProyectos() {
         const res = await fetch(`${API_URL}/publicProjects/${itsonId}`);
         const data = await res.json();
 
-        console.log("Proyectos obtenidos:", data);
+        console.log("Proyectos:", data);
 
         container.innerHTML = "";
 
         data.forEach(proyecto => {
-            // → Usa la imagen REAL del proyecto si existe
-            const urlImagen = proyecto.images && proyecto.images.length > 0
-                ? proyecto.images[0]   // imagen guardada en backoffice
-                : "https://imgs.search.brave.com/MRAvdrhQdD2RfhRBYlZmaYYf2xhGRE_PybpAmebue40/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvYmx1/ZS1zbW9reS1sZXR0/ZXItbi1maDlxa2Rt/d2gzOXl4cnN5Lmpw/Zw";
-
-            // → Si tienes URL de proyecto en la API, úsala
-            const urlProyecto = proyecto.url || "#";
-
             container.innerHTML += `
                 <div class="project-card">
-                    <img src="${urlImagen}"
-                         class="project-image"
-                         alt="Imagen del proyecto">
+                    <img src="${proyecto.images?.[0] || ''}" 
+                         class="project-image">
 
                     <h3>${proyecto.title}</h3>
                     <p>${proyecto.description}</p>
 
-                    <a href="${urlProyecto}" target="_blank">
+                    <a href="${'https://ivirtual.itson.edu.mx/mod/assign/view.php?id=1350150'}" target="_blank">
                         Ver Proyecto
                     </a>
                 </div>
